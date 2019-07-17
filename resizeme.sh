53927220ef253c446b238f9a4da5a74e957e4005
Jefferson Rocha <lrcjefferson@gmail.com>
2019-07-16 22:57:57 -0300

    start repo

---
+++ resizeme.sh
@@ -0,0 +1,88 @@
+#!/usr/bin/env bash
+#======================HEADER============================================|
+#AUTOR
+# Jefferson Rocha <root@slackjeff.com.br>
+#
+#PROGRAMA
+# Redimensiona e cria estrutura de fotos para post em blog de fotografias
+#
+#========================================================================|
+
+thumbs="thumb"
+original="original"
+
+read -ep "Directory for enter: " directory
+if [[ ! -d "$directory" ]]; then
+    echo "Directory dont exist."
+    exit 1
+fi
+pushd "$directory" &>/dev/null
+# Criando diretórios necessários se precisar.
+for dir in "$thumbs" "$original"; do
+	if [[ ! -d "$dir" ]]; then
+		mkdir "$dir"
+	fi
+done
+
+# Gerando cabeçalho
+cat <<EOF > index.html
+<!DOCTYPE HTML>
+    <!--
+         CREATED BY resizemi.sh
+     -->
+ <html>
+ 	<head>
+    	<title>Debconf19</title>
+        <style>
+        	body {background-color: black;font-size: 1.2em; color: #00ff00;margin: 1em auto;max-width: 46em;}
+        	h1{text-align: center;}
+        	hr{margin-bottom: 4%;}
+        	p{color: white;}
+            a:hover{background: #00ff00;}
+        </style>
+    </head>
+    <body>
+   		<header>
+        	<h1>Debconf19</h1>
+            <h2>Curitiba-PR-Brasil</h2>
+        </header>
+    <hr>
+EOF
+
+# Convertendo imagens para thumb
+for x in *; do
+    if [[ "$x" =~ .*\.(jpg|jpeg|png) ]]; then
+        name="${x/%.*/}"
+        extension="${x/#*./}"
+        if convert "$x" -resize 120x90 "${name}-thumb.${extension}"; then
+            echo "<a href="original/${name}.${extension}"><img src="thumb/${name}-thumb.${extension}"></a>" \
+            >> 'index.html'
+		elif [[ $(file "$x") =~ .*(mp4|MP4) ]]; then # videos
+			 ffmp7g  -i "$x" -r 1 -vf scale=-1:120 -vcodec png capture-%002d.png
+        fi
+    fi
+done
+
+# Rodapé
+cat <<EOF >> index.html
+</body>
+</html>
+EOF
+
+
+# Movendo thumbs e fotos originais
+echo "Move ALL THUMBS for '$thumbs'"
+if [[ -d "$thumbs" ]]; then
+	mv *-thumb* "$thumbs" && echo "Moved Thumbs OK" &>/dev/null
+fi
+
+# Movendo arquivos originais
+echo "Move ALL Original photos for '$original'"
+mv *.{jpg,jpeg,png} "$original" &>/dev/null
+
+# Retornando para o diretorio principal.
+popd &>/dev/null
+
+
+# Enviando para o servidor
+rsync -azh ${directory} slackjeff@slackjeff.com.br:public_html/fotos/debconf19
