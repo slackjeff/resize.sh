89e596505d607259594caca175ade2b8333087e2
Jefferson Rocha <root@slackjeff.com.br>
2019-07-31 18:14:56 -0300

    Update

--- resizeme.sh
+++ resizeme.sh
@@ -8,42 +8,48 @@
 #
 #========================================================================|
 
-thumbs="thumb"
-original="original"
+#===========================VARIAVEIS
+# DiretÛrios para enviar imagens prontas
+thumbs="thumb"      # Capa das imagens
+original="original" # Fotos em tamanho real
 
+# Servidor para envio, utilizando o rsync para sincronia
+server="slackjeff@slackjeff.com.br:public_html/fotos/debconf19"
+
+#===========================INICIO
 read -ep "Directory for enter: " directory
 if [[ ! -d "$directory" ]]; then
     echo "Directory dont exist."
     exit 1
 fi
 pushd "$directory" &>/dev/null
-# Criando diret√≥rios necess√°rios se precisar.
+# Criando diretÛrios necess·rios se precisar.
 for dir in "$thumbs" "$original"; do
 	if [[ ! -d "$dir" ]]; then
 		mkdir "$dir"
 	fi
 done
 
-# Gerando cabe√ßalho
+# Gerando cabeÁalho
 cat <<EOF > index.html
 <!DOCTYPE HTML>
     <!--
-         CREATED BY resizemi.sh
+         CREATED BY resizeme.sh
      -->
  <html>
  	<head>
     	<title>Debconf19</title>
         <style>
-        	body {background-color: black;font-size: 1.2em; color: #00ff00;margin: 1em auto;max-width: 46em;}
-        	h1{text-align: center;}
-        	hr{margin-bottom: 4%;}
-        	p{color: white;}
-            a:hover{background: #00ff00;}
+              body {background-color: black;font-size: 1.2em; color: #00ff00;margin: 1em auto;max-width: 46em;}
+              h1{text-align: center;}
+              hr{margin-bottom: 4%;}
+              p{color: white;}
+              a:hover{background: #00ff00;}
         </style>
     </head>
     <body>
-   		<header>
-        	<h1>Debconf19</h1>
+	<header>
+            <h1>Debconf19</h1>
             <h2>Curitiba-PR-Brasil</h2>
         </header>
     <hr>
@@ -61,7 +67,7 @@ for x in *; do
     fi
 done
 
-# Rodap√©
+# RodapÈ
 cat <<EOF >> index.html
 </body>
 </html>
@@ -81,6 +87,5 @@ mv *.{jpg,jpeg,png,mp4} "$original" &>/dev/null
 # Retornando para o diretorio principal.
 popd &>/dev/null
 
-
 # Enviando para o servidor
-rsync -avzh ${directory} slackjeff@slackjeff.com.br:public_html/fotos/debconf19
+rsync -avzh ${directory} "$server"
